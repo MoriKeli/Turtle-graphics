b659c3e4e5695649ace2dde64f38746d6a72bdf2
bro_code <78599959+MoriKeli@users.noreply.github.com>
2022-08-02 16:41:44 +0300

    Add files via upload

---
+++ 16-apex star.py
@@ -0,0 +1,16 @@
+import turtle
+import math
+
+ttl = turtle.Turtle()
+ttl.speed(15)
+ttl.hideturtle()
+ttl.color('red')
+
+ttl.begin_fill()
+for i in range(70):
+    ttl.forward(math.sqrt(i)*10)
+    ttl.left(150)
+
+ttl.end_fill()
+
+turtle.done()
